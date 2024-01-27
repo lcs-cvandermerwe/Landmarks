dec0a05f57b5e88eacc356e28ce2bdb2216902d5
Cari van der Merwe <cvandermerwe@lcs.on.ca>
2024-01-27 09:59:14 -0500

    Created detail view and organized project by creating Views group.

---
+++ Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Landmarks
+//
+//  Created by Cari van der Merwe on 2024-01-27.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
